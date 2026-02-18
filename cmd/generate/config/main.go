1a8cd07e040d82a9a71660bf5a3fe14336e79f64
Zach <15034943+zricethezav@users.noreply.github.com>
2026-02-18 16:28:13 -0600

    bl config

--- cmd/generate/config/main.go
+++ cmd/generate/config/main.go
@@ -15,14 +15,14 @@ const (
 	templatePath = "rules/config.tmpl"
 )
 
-//go:generate go run $GOFILE ../../../config/gitleaks.toml
+//go:generate go run $GOFILE ../../../config/betterleaks.toml
 
 func main() {
 	if len(os.Args) < 2 {
-		_, _ = os.Stderr.WriteString("Specify path to the gitleaks.toml config\n")
+		_, _ = os.Stderr.WriteString("Specify path to the betterleaks.toml config\n")
 		os.Exit(2)
 	}
-	gitleaksConfigPath := os.Args[1]
+	betterleaksConfigPath := os.Args[1]
 
 	configRules := []*config.Rule{
 		rules.OnePasswordSecretKey(),
@@ -283,7 +283,7 @@ func main() {
 		logging.Fatal().Err(err).Msg("Failed to parse template")
 	}
 
-	f, err := os.Create(gitleaksConfigPath)
+	f, err := os.Create(betterleaksConfigPath)
 	if err != nil {
 		logging.Fatal().Err(err).Msg("Failed to create rules.toml")
 	}
